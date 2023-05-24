8769b56939a5f719489de9bc64ffb725a1ec75a1
konnovk <konnovki@yandex.ru>
2023-05-25 01:49:07 +0300

    feat: list LogItems handler

--- api/routers/log_item.py
+++ api/routers/log_item.py
@@ -8,6 +8,7 @@ from sqlalchemy.exc import IntegrityError
 
 from api.dependencies import get_db, check_auth
 from api.schemas.log_item import CreateLogItemRequest, LogItemResponse
+from api.globals import log_levels
 
 from db import DB, User, LogGroup, LogItem, LogLevel
 
@@ -55,5 +56,31 @@ async def delete_log_item(id: uuid.UUID, db: DB = Depends(get_db), user_id: str 
 
 
 @router.get('')
-async def list_log_items(db: DB = Depends(get_db), user_id: str = Depends(check_auth)) -> list[LogItemResponse]:
-    raise HTTPException(501)
+async def list_log_items(
+    log_group_id: uuid.UUID,
+    level: log_levels | None = None,
+    limit: int | None = None,
+    offset: int | None = None,
+    db: DB = Depends(get_db),
+    user_id: str = Depends(check_auth)
+) -> list[LogItemResponse]:
+    async with db.async_session() as session:
+        session: AsyncSession
+        user_db = await session.scalar(sa.select(User).where(User.id == user_id))
+        if user_db is None:
+            raise HTTPException(401, "wrond access token")
+        log_group = await session.scalar(sa.select(LogGroup).where(User.id == user_id))
+        if log_group is None:
+            raise HTTPException(403, "you cannot read logs from this group")
+        
+        stmt = sa.select(LogItem).where(LogGroup.id == log_group_id)
+
+        if level is not None:
+            stmt = stmt.where(LogLevel.name == level)
+        if limit is not None:
+            stmt = stmt.limit(limit)
+        if offset is not None:
+            stmt = stmt.offset(offset)
+
+        log_items = (await session.execute(stmt)).scalars().all()
+        return log_items
