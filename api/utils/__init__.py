896091bf3ed154903c5a98f760334c3874e61723
konnovk <konnovki@yandex.ru>
2023-05-29 23:48:01 +0300

    refactor: delete useless code

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,11 +1,6 @@
-from .db import create_all_log_levels, create_log_level
-from .hash import hash_password
 from .jwt import create_jwt, check_access_token_unexpired, get_id_from_access_token
 
 all = [
-    create_all_log_levels,
-    create_log_level,
-    hash_password,
     create_jwt,
     check_access_token_unexpired,
     get_id_from_access_token,
--- api/utils/hash.py
+++
@@ -1,5 +0,0 @@
-from hashlib import md5
-
-
-def hash_password(email: str, password: str) -> str:
-    return md5(f'{email}:{password}'.encode()).hexdigest()
