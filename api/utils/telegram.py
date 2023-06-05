7fd0038fd01b0319ac877800b06764f270b193ca
konnovk <konnovki@yandex.ru>
2023-06-06 00:42:18 +0300

    fix

--- api/utils/telegram.py
+++ api/utils/telegram.py
@@ -17,7 +17,7 @@ async def multicast_message(text: str):
 
 
 async def multicast_log_item(level: str, timestamp: datetime.datetime, message: str):
-    if level.lower() not in settings.telegram_levels:
+    if level.upper() not in settings.telegram_levels:
         return
     await multicast_message(f'{timestamp}\n{level}\n\n{message}')
 
