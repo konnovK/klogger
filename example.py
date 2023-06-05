2815e389b8627a87053d10a07bf834f24f1a0f57
konnovk <konnovki@yandex.ru>
2023-06-05 12:18:56 +0300

    fix: log format in example

--- example.py
+++ example.py
@@ -2,10 +2,11 @@ import random
 import uuid
 from loguru import logger
 from clients.klogger import KLoggerHandler
+import time
 
 handler = KLoggerHandler('185.221.213.55:8080', 'konnovki@yandex.ru', '123', '3a9e46a0-3568-454b-a5ba-c825eeeca219')
 
-logger.add(handler, enqueue=True, level='DEBUG')
+logger.add(handler, enqueue=True, level='DEBUG', format='<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - {message}')
 
 i = 1
 while True:
@@ -13,3 +14,4 @@ while True:
     log_message = f'Second test obj {i}: {uuid.uuid4()}'
     logger.log(log_level, log_message)
     i += 1
+    time.sleep(0.333)
